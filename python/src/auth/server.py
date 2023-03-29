3c5d0fe3250a121e33ea27fd601ab89afeb0a7d9
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2023-03-29 20:17:26 +0530

    docker

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -38,11 +38,36 @@ def login():
         return "Invalid credentials", 401
 
 
-def createJWT(username, secret, is_admin):
-    payload = {
-        "username": username,
-        "is_admin": is_admin,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
-    }
-    token = jwt.encode(payload, secret, algorithm="HS256")
-    return jsonify({"token": token.decode("UTF-8")})
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers.get("Authorization")
+
+    if not encoded_jwt:
+        return "Missing credentials", 401
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded_jwt = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+        return decoded_jwt, 200
+    except jwt.ExpiredSignatureError:
+        return "Not authorized", 403
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
